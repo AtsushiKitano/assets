e640937b333f651224ac57d11b7c59ec34df2230
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-03-12 23:58:01 +0900

    [mod] vpc network moduleのアウトプットの修正

--- terraform/gcp/modules/network/vpc_network/output.tf
+++ terraform/gcp/modules/network/vpc_network/output.tf
@@ -1,19 +1,5 @@
-output "serverless_vpc_self_link" {
-  value = {
-    for v in var.serverless_vpc : v.name => google_vpc_access_connector.main[v.name].self_link
-  }
-}
-
-output "serverless_vpc_name" {
-  value = {
-    for v in var.serverless_vpc : v.name => google_vpc_access_connector.main[v.name].name
-  }
-}
-
-output "serverless_vpc_id" {
-  value = {
-    for v in var.serverless_vpc : v.name => google_vpc_access_connector.main[v.name].id
-  }
+output "self_link" {
+  value = google_compute_network.main.self_link
 }
 
 output "subnetwork_self_link" {
@@ -22,40 +8,24 @@ output "subnetwork_self_link" {
   }
 }
 
-output "subnetwork_id" {
+output "serverless_vpc_self_link" {
   value = {
-    for v in var.subnetworks : v.name => google_compute_subnetwork.main[v.name].id
+    for v in var.serverless_vpc : v.name => google_vpc_access_connector.main[v.name].self_link
   }
 }
 
-output "subnetwork_gateway_address" {
+output "vpc" {
+  value = google_compute_network.main
+}
+
+output "subnetwork" {
   value = {
-    for v in var.subnetworks : v.name => google_compute_subnetwork.main[v.name].gateway_address
+    for v in var.subnetworks : v.name => google_compute_subnetwork.main[v.name]
   }
-}
-
-output "subnetwork_name" {
-  value = [
-    for v in var.subnetworks : v.name
-  ]
-}
-
-output "self_link" {
-  value = google_compute_network.main.self_link
-}
-
-output "id" {
-  value = google_compute_network.main.id
-}
-
-output "gateway_ipv4" {
-  value = google_compute_network.main.gateway_ipv4
-}
-
-output "name" {
-  value = google_compute_network.main.name
 }
 
 output "firewall" {
-  value = google_compute_firewall.main
+  value = {
+    for v in var.firewall : v.name => google_compute_firewall.main[v.name]
+  }
 }
