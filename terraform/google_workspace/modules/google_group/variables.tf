d71e94916fdebc0675a88fe2cb6b0afc3342bd05
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-09-26 19:43:30 +0900

    [add] workspace module codes

---
+++ terraform/google_workspace/modules/google_group/variables.tf
@@ -0,0 +1,28 @@
+variable "emaile" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type    = string
+  default = null
+}
+
+variable "members" {
+  type = list(object({
+    email = string
+    role  = string
+  }))
+  default = []
+}
+
+variable "timeouts" {
+  type = object({
+    create = string
+    update = string
+  })
+  default = null
+}
