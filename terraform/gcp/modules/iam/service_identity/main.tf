e4e5d76acb64d6ef778e0676b9cf909304c8a80a
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-09-26 18:35:38 +0900

    [mod] serviceの変数名を修正

--- terraform/gcp/modules/iam/service_identity/main.tf
+++ terraform/gcp/modules/iam/service_identity/main.tf
@@ -1,22 +1,26 @@
+locals {
+  _service = "${var.service}.googleapis.com"
+}
+
 resource "google_project_service_identity" "main" {
-  for_each = var.service != "bigquery.googleapis.com" && var.service != "storage.googleapis.com" && var.service != "compute.googleapis.com" ? toset(["enable"]) : []
+  for_each = local._service != "bigquery.googleapis.com" && local._service != "storage.googleapis.com" && local._service != "compute.googleapis.com" ? toset(["enable"]) : []
   provider = google-beta
 
   project = var.project_id
-  service = var.service
+  service = local._service
 }
 
 data "google_bigquery_default_service_account" "main" {
-  for_each = var.service == "bigquery.googleapis.com" ? toset(["enable"]) : []
+  for_each = local._service == "bigquery.googleapis.com" ? toset(["enable"]) : []
   project  = var.project_id
 }
 
 data "google_storage_project_service_account" "main" {
-  for_each = var.service == "storage.googleapis.com" ? toset(["enable"]) : []
+  for_each = local._service == "storage.googleapis.com" ? toset(["enable"]) : []
   project  = var.project_id
 }
 
 data "google_project" "main" {
-  for_each   = var.service == "compute.googleapis.com" ? toset(["enable"]) : []
+  for_each   = local._service == "compute.googleapis.com" ? toset(["enable"]) : []
   project_id = var.project_id
 }
--- terraform/gcp/modules/iam/service_identity/outputs.tf
+++ terraform/gcp/modules/iam/service_identity/outputs.tf
@@ -1,3 +1,3 @@
 output "email" {
-  value = var.service == "bigquery.googleapis.com" ? data.google_bigquery_default_service_account.main.enable.email : var.service == "storage.googleapis.com" ? google_storage_project_service_account.main.enable.email : var.service == "compute.googleapis.com" ? "service-${data.google_project.main.enable.number}@compute-system.iam.gserviceaccount.com" : google_project_service_identity.main.enable.email
+  value = local._service == "bigquery.googleapis.com" ? data.google_bigquery_default_service_account.main.enable.email : local._service == "storage.googleapis.com" ? google_storage_project_service_account.main.enable.email : local._service == "compute.googleapis.com" ? "service-${data.google_project.main.enable.number}@compute-system.iam.gserviceaccount.com" : google_project_service_identity.main.enable.email
 }
