36b7ffded6a7295bc35b6d1569738211dc70fb94
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-09-26 15:06:56 +0900

    [mod] 三項演算子の修正

--- terraform/gcp/modules/project/folder/main.tf
+++ terraform/gcp/modules/project/folder/main.tf
@@ -11,14 +11,14 @@ resource "google_folder" "child_folder" {
 }
 
 resource "google_folder" "gland_child_folder" {
-  for_each = if(length(var.child_folders) > 0) ? { for v in var.gland_child_folders : "${v.parent_folder_name}/${v.name}" => v } : {}
+  for_each = length(var.child_folders) > 0 ? { for v in var.gland_child_folders : "${v.parent_folder_name}/${v.name}" => v } : {}
 
   display_name = each.value.name
   parent       = join("/", ["organizations", google_folder.child_folder[each.value.parent_folder_name].id])
 }
 
 resource "google_folder" "gland_glandson_folder" {
-  for_each = if(length(var.gland_child_folders) > 0 && length(var.child_folders) > 0) ? { for v in var.gland_glandson_folders : "${v.parent_folder_name}/${v.name}" => v } : {}
+  for_each = length(var.gland_child_folders) > 0 && length(var.child_folders) > 0 ? { for v in var.gland_glandson_folders : "${v.parent_folder_name}/${v.name}" => v } : {}
 
   display_name = each.value.name
   parent       = join("/", ["organizations", google_folder.gland_child_folder[each.value.parent_folder_name].id])
