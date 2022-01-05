1d5e099894e167cc08dc5ca48e0c8a3682694716
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-01-05 23:38:51 +0900

    update

--- terraform/aws/modules/network/vpc/main.tf
+++ terraform/aws/modules/network/vpc/main.tf
@@ -29,7 +29,7 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_security_group" "main" {
-  for_each = var.security_group != null ? toset([var.security_group.name]) : []
+  for_each = var.security_group != null ? toset(["enable"]) : []
 
   name   = var.security_group.name
   vpc_id = aws_vpc.main.id
@@ -44,5 +44,5 @@ resource "aws_security_group_rule" "main" {
   from_port         = var.security_group.rules[count.index].from_port
   to_port           = var.security_group.rules[count.index].to_port
   protocol          = var.security_group.rules[count.index].protocol
-  security_group_id = aws_security_group.main[var.security_group.name].id
+  security_group_id = aws_security_group.main["enable"].id
 }
