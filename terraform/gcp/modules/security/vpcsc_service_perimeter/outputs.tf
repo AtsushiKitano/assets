b144d047d69d3dadcc6c4fef151352a07ef7ea02
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-01-05 10:22:46 +0900

    add number

--- terraform/gcp/modules/security/vpcsc_service_perimeter/outputs.tf
+++ terraform/gcp/modules/security/vpcsc_service_perimeter/outputs.tf
@@ -1,3 +1,3 @@
 output "project_number" {
-  value = var.status != null ? { for v in var.status.projects : v => data.google_project.main[v].number if contains(google_access_context_manager_service_perimeter.main.status.resources, format("projects/%d", data.google_project.main[v].number)) } : {}
+  value = var.status != null ? { for v in var.status.projects : v => data.google_project.main[v].number if contains(google_access_context_manager_service_perimeter.main.status[0].resources, format("projects/%d", data.google_project.main[v].number)) } : {}
 }
