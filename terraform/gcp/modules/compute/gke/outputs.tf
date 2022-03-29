d2d59d1f3380437405f792fa5a953e9ca666bd6f
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-03-29 11:00:13 +0900

    [mod] gke cluster output file

---
+++ terraform/gcp/modules/compute/gke/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_id" {
+  value = google_container_cluster.main.id
+}
+
+output "cluster" {
+  value = google_container_cluster.main
+}
+
+output "nodes" {
+  value = { for v in var.node_pools : v.name => google_container_node_pool.main[v.name] if var.enable_autopolot == null }
+}
