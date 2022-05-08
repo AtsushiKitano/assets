41978ca7efed1cdada6de4fb30d94b1ce95328aa
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-05-08 15:10:51 +0900

    update

--- terraform/gcp/modules/security/acm/main.tf
+++ terraform/gcp/modules/security/acm/main.tf
@@ -1,7 +1,12 @@
 locals {
   members = concat(
-    flatten([for v in var.conditions : [for w in v.users : format("user:%s", w)]]),
-    flatten([for v in var.conditions : [for w in v.service_accounts : format("serviceAccount:%s", w)]])
+    [for v in var.users : format("user:%s", v)],
+    [for v in var.service_accounts : format("serviceAccount:%s", v)],
+  )
+
+  additional_members = concat(
+    flatten([for v in var.additional_conditions : [for w in v.users : format("user:%s", w)]]),
+    flatten([for v in var.additional_conditions : [for w in v.service_accounts : format("serviceAccount:%s", w)]]),
   )
 }
 
@@ -9,13 +14,23 @@ resource "google_access_context_manager_access_level" "main" {
   parent = format("accessPolicies/%s", var.parent)
   name   = format("accessPolicies/%s/accessLevels/%s", var.parent, var.title)
   title  = var.title
+
+  basic {
+    combining_function = var.combining_function
+
+    conditions {
+      ip_subnetworks = var.ip_addresses
+      members        = local.members
+      regions        = var.regions
+    }
+  }
 }
 
 resource "google_access_context_manager_access_level_condition" "main" {
-  count        = length(var.conditions)
+  count        = length(var.additional_conditions)
   access_level = google_access_context_manager_access_level.main.name
 
   ip_subnetworks = var.conditions[count.index].ip_addresses
-  members        = local.members
+  members        = local.additional_members
   regions        = var.conditions[count.index].regions
 }
--- terraform/gcp/modules/security/acm/varibale.tf
+++ terraform/gcp/modules/security/acm/varibale.tf
@@ -6,11 +6,37 @@ variable "title" {
   type = string
 }
 
-variable "conditions" {
+variable "combining_function" {
+  type    = string
+  default = "AND"
+}
+
+variable "ip_addresses" {
+  type    = list(string)
+  default = []
+}
+
+variable "users" {
+  type    = list(string)
+  default = []
+}
+
+variable "service_accounts" {
+  type    = list(string)
+  default = []
+}
+
+variable "regions" {
+  type    = list(string)
+  default = []
+}
+
+variable "additional_conditions" {
   type = list(object({
     ip_addresses     = list(string)
     service_accounts = list(string)
     users            = list(string)
     regions          = list(string)
   }))
+  default = []
 }
