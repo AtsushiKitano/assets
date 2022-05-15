c47cf76d7bccba33f4c5afa7dd6550ac6446b017
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-05-15 11:25:42 +0900

    update

--- terraform/gcp/modules/network/psc/main.tf
+++ terraform/gcp/modules/network/psc/main.tf
@@ -13,6 +13,6 @@ resource "google_compute_forwarding_rule" "main" {
   target                = var.target
   network               = var.network
   region                = var.region
-  ip_address            = google_compute_global_address.main.id
+  ip_address            = google_compute_address.main.id
   load_balancing_scheme = var.load_balancing_scheme
 }
