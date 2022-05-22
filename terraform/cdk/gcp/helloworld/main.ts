7b0abf02814d159af840a5198ed6b852d6656f83
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-05-22 22:12:38 +0900

    update

---
+++ terraform/cdk/gcp/helloworld/main.ts
@@ -0,0 +1,20 @@
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+import {ComputeNetwork,GoogleProvider} from "@cdktf/provider-google"
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new GoogleProvider(this, "google", {});
+    
+    new ComputeNetwork(this, "vpcResarch", {
+      name: "sample123",
+      project: "ca-kitano-study-sandbox",
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "helloworld");
+app.synth();
