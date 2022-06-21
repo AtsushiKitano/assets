60a5c33bbd017967235890e26a7909b80b820888
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-06-21 12:54:18 +0900

    add tcp lb module

---
+++ terraform/gcp/modules/network/tcp_lb/main.tf
@@ -0,0 +1,62 @@
+resource "google_compute_address" "main" {
+  name         = var.name
+  project      = var.project
+  address_type = var.address_type
+  purpose      = "SHARED_LOADBALANCER_VIP"
+  network_tier = var.network_tier
+  region       = var.address_region
+}
+
+resource "google_compute_region_backend_service" "main" {
+  name     = var.name
+  project  = var.project
+  region   = var.region
+  network  = var.network
+  protocol = var.protocol
+
+  load_balancing_scheme = var.load_balancing_scheme
+  health_checks = [
+    google_compute_region_health_check.main.id
+  ]
+
+  dynamic "backend" {
+    for_each = [for v in var.backends : v]
+    iterator = _conf
+
+    content {
+      group          = _conf.value.group
+      failover       = _conf.value.failover
+      balancing_mode = _conf.value.balancing_mode
+    }
+  }
+}
+
+resource "google_compute_region_health_check" "main" {
+  name = var.helth_check_name
+
+  timeout_sec        = var.timeout_sec
+  check_interval_sec = var.check_interval_sec
+  project            = var.project
+  region             = var.region
+
+  tcp_health_check {
+    port = var.port
+  }
+}
+
+resource "google_compute_target_tcp_proxy" "main" {
+  name            = var.name
+  backend_service = google_compute_region_backend_service.main.id
+  project         = var.project
+}
+
+resource "google_compute_forwarding_rule" "main" {
+  name                  = var.name
+  ip_address            = google_compute_address.main.id
+  network               = var.network
+  network_tier          = var.network_tier
+  ip_protocol           = var.ip_protocol
+  region                = var.region
+  load_balancing_scheme = var.load_balancing_scheme
+  port_range            = var.port_range
+}
---
+++ terraform/gcp/modules/network/tcp_lb/variable.tf
@@ -0,0 +1,77 @@
+variable "name" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "address_type" {
+  type = string
+}
+
+variable "network_tier" {
+  type    = string
+  default = "STANDARD"
+}
+
+variable "address_region" {
+  type    = string
+  default = null
+}
+
+variable "network" {
+  type = string
+}
+
+variable "protocol" {
+  type = string
+}
+
+variable "load_balancing_scheme" {
+  type    = string
+  default = "EXTERNAL"
+
+  validation {
+    condition     = var.load_balancing_scheme == "EXTERNAL" || var.load_balancing_scheme == "EXTERNAL_MANAGED" || var.load_balancing_scheme == "INTERNAL" || var.load_balancing_scheme == "INTERNAL_MANAGED"
+    error_message = "the load_balancing_scheme must be EXTERNAL, EXTERNAL_MANAGED, INTERNAL or INTERNAL_MANAGED."
+  }
+}
+
+variable "backends" {
+  type = list(object({
+    group          = string
+    failover       = string
+    balancing_mode = string
+  }))
+}
+
+variable "helth_check_name" {
+  type = string
+}
+
+variable "timeout_sec" {
+  type    = number
+  default = 1
+}
+
+variable "check_interval_sec" {
+  type    = number
+  default = 1
+}
+
+variable "port" {
+  type = string
+}
+
+variable "ip_protocol" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "port_range" {
+  type = string
+}
