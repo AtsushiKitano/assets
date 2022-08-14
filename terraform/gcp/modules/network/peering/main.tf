8d586b3d46e0ae9b750f4371fd98cff59862273b
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-08-14 12:44:35 +0900

    service peeringのモジュール改修

--- terraform/gcp/modules/network/peering/main.tf
+++ terraform/gcp/modules/network/peering/main.tf
@@ -1,26 +1,16 @@
-locals {
-  networks = [
-    {
-      name         = var.peering.main_name
-      network      = var.peering.network[0]
-      peer_network = var.peering.network[1]
-    },
-    {
-      name         = var.peering.sub_name
-      network      = var.peering.network[1]
-      peer_network = var.peering.network[0]
-    },
-  ]
+resource "google_service_networking_connection" "main" {
+  network                 = var.network
+  service                 = var.service
+  reserved_peering_ranges = google_compute_global_address.main.*.name
 }
 
-resource "google_compute_network_peering" "main" {
-  for_each = { for v in local.networks : v.name => v }
+resource "google_compute_global_address" "main" {
+  for_each = { for v in var.addresses : v.name => v }
 
-  name                                = each.value.name
-  network                             = each.value.network
-  peer_network                        = each.value.peer_network
-  export_custom_routes                = var.export_custom_routes != null ? lookup(var.export_custom_routes, each.value.network, false) : false
-  import_custom_routes                = var.import_custom_routes != null ? lookup(var.import_custom_routes, each.value.network, false) : false
-  export_subnet_routes_with_public_ip = var.export_subnet_routes_with_public_ip != null ? lookup(var.export_subnet_routes_with_public_ip, each.value.network, true) : true
-  import_subnet_routes_with_public_ip = var.import_subnet_routes_with_public_ip != null ? lookup(var.import_subnet_routes_with_public_ip, each.value.network, false) : false
+  name          = each.value.name
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  address       = split("/", each.value.address)[0]
+  prefix_length = split("/", each.value.address)[1]
+  network       = var.network
 }
--- terraform/gcp/modules/network/peering/variable.tf
+++ terraform/gcp/modules/network/peering/variable.tf
@@ -1,27 +1,14 @@
-variable "peering" {
-  type = object({
-    main_name = string
-    sub_name  = string
-    network   = list(string)
-  })
+variable "network" {
+  type = string
 }
 
-variable "export_custom_routes" {
-  type    = map(bool)
-  default = null
+variable "service" {
+  type = string
 }
 
-variable "import_custom_routes" {
-  type    = map(bool)
-  default = null
-}
-
-variable "export_subnet_routes_with_public_ip" {
-  type    = map(bool)
-  default = null
-}
-
-variable "import_subnet_routes_with_public_ip" {
-  type    = map(bool)
-  default = null
+variable "addresses" {
+  type = list(object({
+    name    = string
+    address = string
+  }))
 }
