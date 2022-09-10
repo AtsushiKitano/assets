d3970b2bfadbfd89ff0dff8a3ee519852dad94c3
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-09-10 20:18:29 +0900

    update

---
+++ terraform/gcp/modules/cicd/cloudbuild_terraform_handtasks/main.tf
@@ -0,0 +1,69 @@
+locals {
+  triggers = [
+    {
+      name = format("%s-terraform-apply", var.name)
+      file = var.terraform_apply_file
+    },
+    {
+      name = format("%s-terraform-plan", var.name)
+      file = var.terraform_plan_file
+    },
+    {
+      name = format("%s-terraform-destroy", var.name)
+      file = var.terraform_destroy_file
+    },
+  ]
+}
+
+resource "google_cloudbuild_trigger" "main" {
+  for_each = { for v in local.triggers : v.name => v if !var.disabled_plan_task && v.name != format("%s-terraform-plan") }
+
+  name            = each.value.name
+  project         = var.project
+  service_account = data.google_service_account.main.id
+
+  source_to_build {
+    uri       = format("https://github.com/%s/%s", var.repo.owner, var.repo.name)
+    ref       = var.ref
+    repo_type = var.repo_type
+  }
+
+  git_file_source {
+    path      = format("%s/%s", var.cloud_build_dir_path, each.value.file)
+    uri       = format("https://github.com/%s/%s", var.repo.owner, var.repo.name)
+    revision  = var.ref
+    repo_type = var.repo_type
+  }
+
+  substitutions = var.substitutions
+}
+
+data "google_service_account" "main" {
+  account_id = var.service_account
+  project    = var.project
+}
+
+data "google_storage_bucket" "main" {
+  name = format("%s-%s", var.project, var.log_bucket)
+}
+
+resource "google_cloud_scheduler_job" "main" {
+  for_each = var.enabled_delete_scheduler_job ? toset([format("%s-terraform-destroy", var.name)]) : []
+
+  name      = format("%s-daily-job", each.value)
+  schedule  = var.schedule
+  time_zone = var.time_zone
+  project   = google_cloudbuild_trigger.main[each.value].project
+  region    = var.region
+
+  http_target {
+    http_method = "POST"
+    uri         = format("https://cloudbuild.googleapis.com/v1/projects/%s/locations/global/triggers/%s:run", google_cloudbuild_trigger.main[each.value].project, google_cloudbuild_trigger.main[each.value].trigger_id)
+    body        = var.body
+
+    oauth_token {
+      service_account_email = data.google_service_account.main.email
+      scope                 = "https://www.googleapis.com/auth/cloud-platform"
+    }
+  }
+}
---
+++ terraform/gcp/modules/cicd/cloudbuild_terraform_handtasks/variable.tf
@@ -0,0 +1,85 @@
+/*
+  Required Configs
+*/
+
+variable "name" {
+  type = string
+}
+
+variable "cloud_build_dir_path" {
+  type = string
+}
+
+variable "repo" {
+  type = object({
+    owner = string
+    name  = string
+  })
+}
+
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+/*
+  Option Configs
+*/
+
+variable "repo_type" {
+  type    = string
+  default = "GITHUB"
+}
+
+variable "ref" {
+  type    = string
+  default = "refs/heads/main"
+}
+
+variable "log_bucket" {
+  type    = string
+  default = "terraform-log"
+}
+
+variable "schedule" {
+  type    = string
+  default = "0 22 * * *"
+}
+
+variable "time_zone" {
+  type    = string
+  default = "Asia/Tokyo"
+}
+
+variable "body" {
+  type    = string
+  default = "eyJzb3VyY2UiOnsiYnJhbmNoTmFtZSI6Im1haW4ifX0="
+}
+
+variable "service_account" {
+  type    = string
+  default = "cloud-build-tf-trigger"
+}
+
+variable "terraform_apply_file" {
+  type    = string
+  default = "terraform_apply.yaml"
+}
+
+variable "terraform_plan_file" {
+  type    = string
+  default = "terraform_plan.yaml"
+}
+
+variable "terraform_destroy_file" {
+  type    = string
+  default = "terraform_destroy.yaml"
+}
+
+variable "substitutions" {
+  type    = map(string)
+  default = null
+}
